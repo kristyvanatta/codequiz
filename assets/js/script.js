0d6e975cb6373878eba69ca11418d60c45608f0b
Kristy Vanatta <kristylvanatta@gmail.com>
2022-09-08 15:33:26 -0400

    added some javascript funtions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,120 @@
+var quiz = document.getElementById('quiz');
+var reults = document.getElementById('results');
+var submitButton 
+
 var quizQuestions = [
     {
         question: "JavaScript is an ______ language?",
         answers: {
-            1: 'Object-Oriented',
-            2: 'Object-Based',
-            3: 'Procedural',
-            4: 'None of the Above'
+            A: 'Object-Oriented',
+            B: 'Object-Based',
+            C: 'Procedural',
+            D: 'None of the Above'
         },
-        correctAnswer: '1'
+        correctAnswer: 'A'
     },
 
     {
         question: "Which of the following keywords is used to define a variable in JavaScript?",
         answers: {
-            1: 'var',
-            2: 'let',
-            3: 'Both 1 and 2',
-            4: 'None of the Above'
+            A: 'var',
+            B: 'let',
+            C: 'Both 1 and 2',
+            D: 'None of the Above'
         },
-        correctAnswer: '3'
+        correctAnswer: 'C'
     },
 
     {
         question: "Which is the following methods is used to access HTML elements using JavaScript?",
         answers: {
-            1: 'getElementById()',
-            2: 'getElementByClassName()',
-            3: 'Both 1 and 2',
-            4: 'None of the Above'
+            A: 'getElementById()',
+            B: 'getElementByClassName()',
+            C: 'Both 1 and 2',
+            D: 'None of the Above'
         },
-        correctAnswer: '3'
+        correctAnswer: 'C'
     },
 
     {
         question: "Upon encountering empty statements, what does the JavaScript interpreter do?",
         answers: {
-            1: 'throws an error',
-            2: 'ignores the statements',
-            3: 'gives a warning',
-            4: 'None of the Above'
+            A: 'throws an error',
+            B: 'ignores the statements',
+            C: 'gives a warning',
+            D: 'None of the Above'
         },
-        correctAnswer: '2'
+        correctAnswer: 'B'
     },
 
     {
         question: "Which of the following of the following methods can be used to display data in some form using JavaScript?",
         answers: {
-            1: 'document.write()',
-            2: 'console.log()',
-            3: 'window.alert()',
-            4: 'All of the Above'
+            A: 'document.write()',
+            B: 'console.log()',
+            C: 'window.alert()',
+            D: 'All of the Above'
         },
-        correctAnswer: '4'
+        correctAnswer: 'D'
     },
 
     {
         question: "How can a data type be declared to be a constant type?",
         answers: {
-            1: 'const',
-            2: 'let',
-            3: 'var',
-            4: 'constatnt'
+            A: 'const',
+            B: 'let',
+            C: 'var',
+            D: 'constatnt'
         },
-        correctAnswer: '1'
+        correctAnswer: 'A'
     },
 ];
+
+function generateQuiz(questions, quiz, results, submitButton) {
+    function showQuestions (questions, quiz) {
+        var output = [];
+        var answers;
+
+        for(var i=0; i<questions.length; i++){
+            answers = [];
+
+        for(letter in questions[i].answers){
+            answers.push(
+                '<label>'
+                + '<input type="button" name="question'+i+'" value="'+letter+'">'
+                + letter + ': '
+                + questions[i].answers[letter]
+                + '</label>'
+            );
+        }
+
+        }
+
+    }
+    function showResults(questions, quiz, results) {
+        var answers = quiz.querySelectorAll('.answers');
+        var userAnswer = '';
+        var numCorrect = 0;
+
+        for(var i=0; i<questions.length; i++){
+            userAnswer = (answers[i].querySelector('input[neme=question'+i+']:clicked')||{}).value;
+
+            if(userAnswer===questions[i].correctAnswer){
+                numCorrect++;
+                answer[i].style.color = 'green';
+            }
+
+            else{
+                answer.[i].style.color = 'red';
+            }
+        }
+
+        results.innerHTML = numCorrect + ' out of ' + questions.length;
+
+    }
+    showQuestions(question, quiz);
+
+    submitButton.onclick = function(){
+        showResults(questions, quiz, results);
+    }
+}
