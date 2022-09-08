cc0972653c06dfe41a86ac2fd24f38b71927582a
Kristy Vanatta <kristylvanatta@gmail.com>
2022-09-08 13:24:35 -0400

    added the questions and some basic HTML

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+var quizQuestions = [
+    {
+        question: "JavaScript is an ______ language?",
+        answers: {
+            1: 'Object-Oriented',
+            2: 'Object-Based',
+            3: 'Procedural',
+            4: 'None of the Above'
+        },
+        correctAnswer: '1'
+    },
+
+    {
+        question: "Which of the following keywords is used to define a variable in JavaScript?",
+        answers: {
+            1: 'var',
+            2: 'let',
+            3: 'Both 1 and 2',
+            4: 'None of the Above'
+        },
+        correctAnswer: '3'
+    },
+
+    {
+        question: "Which is the following methods is used to access HTML elements using JavaScript?",
+        answers: {
+            1: 'getElementById()',
+            2: 'getElementByClassName()',
+            3: 'Both 1 and 2',
+            4: 'None of the Above'
+        },
+        correctAnswer: '3'
+    },
+
+    {
+        question: "Upon encountering empty statements, what does the JavaScript interpreter do?",
+        answers: {
+            1: 'throws an error',
+            2: 'ignores the statements',
+            3: 'gives a warning',
+            4: 'None of the Above'
+        },
+        correctAnswer: '2'
+    },
+
+    {
+        question: "Which of the following of the following methods can be used to display data in some form using JavaScript?",
+        answers: {
+            1: 'document.write()',
+            2: 'console.log()',
+            3: 'window.alert()',
+            4: 'All of the Above'
+        },
+        correctAnswer: '4'
+    },
+
+    {
+        question: "How can a data type be declared to be a constant type?",
+        answers: {
+            1: 'const',
+            2: 'let',
+            3: 'var',
+            4: 'constatnt'
+        },
+        correctAnswer: '1'
+    },
+];
